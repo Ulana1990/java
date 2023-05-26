e1dec191c8b361bb159575155ad754ef0d82822c
UlianaPonaida <130707470+Ulana1990@users.noreply.github.com>
2023-05-26 23:37:29 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,92 @@
+
+let firstName = "Uliana";
+let lastName = "Ponaida";
+    console.log(firstName+lastName);
+
+let width = 13;
+let height = 7;
+let P = (width+height) *2;
+    console.log(P= "40");
+
+const result = Math.pow(10, 6);
+    console.log(result); 
+
+
+    console.log(Math.floor(Math.random() * 50));
+    
+
+let number = 10; 
+if ( number > 0)
+    console.log('negative value');
+ else  {
+    console.log('positive value');
+}
+
+let text = 'Java';
+if ( text.length > 0);
+    console.log(text[3]);
+
+let month = 11;
+ switch ( month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("May")
+        break;
+    case 6:
+        console.log("June")
+        break;
+    case 7:
+        console.log("July ")
+        break;
+    case 8:
+        console.log("August")
+        break;
+    case 9:
+        console.log("September")
+        break;
+    case 10:
+        console.log("October")
+        break;
+    case 11:
+        console.log("November")
+        break;
+    case 12 :
+        console.log("December") 
+        break;  
+ }
+
+let password = "adidas";
+if( password.length > 6 && password.indexOf("") ===-1 ) {
+    console.log('this password is strong');
+} else {
+    console.log('this password is weak');
+}
+
+let a= 6;
+let b= 8;
+let suma;
+suma=a+b;
+    console.log(suma); 
+
+
+let textOne = "Bob"; 
+let textTwo = "Tom";
+if (textOne.length ===textTwo.length ) ; {
+    console.log ('those items are strings and they\’re equal');
+} { 
+    console.log ('items are not compareble');
+}
+
+
+    
